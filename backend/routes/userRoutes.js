1b7708b6e5bbe0024e2d63042751aea491c4495a
Ondřej Zeinert <zeinert@glancemedia.local>
2023-08-18 15:59:08 +0200

    created first routes and controllers

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {authUser, 
+        registerUser, 
+        logoutUser, 
+        getUserProfile, 
+        updateUserProfile} from "../controllers/userController.js"
+
+const router = express.Router();
+
+router.post(`/auth`, authUser)
+router.post(`/`, registerUser)
+router.post(`/logout`, logoutUser)
+router.get(`/profile`, getUserProfile)
+router.route(`/profile`).get(getUserProfile).put(updateUserProfile);
+
+
+
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+
 dotenv.config();
-
 const port = process.env.PORT 
-
 const app = express();
 
+
+app.use("/api/users", userRoutes)
+
 app.get(`/`, (req,res) => res.send(`server is running`))
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`))

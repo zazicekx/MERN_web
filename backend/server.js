9078ad115dc1486f162821d13d26ce18bf5df3ac
Ondřej Zeinert <zeinert@glancemedia.local>
2023-08-21 17:12:41 +0200

    did all to like 1:06, but having error about headers

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const port = process.env.PORT 
 const app = express();
+connectDB();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes)
 

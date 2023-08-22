c37cc69216115ff9ae860e1973c29ba746644903
Ondřej Zeinert <zeinert@glancemedia.local>
2023-08-22 12:11:33 +0200

    finished backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -89,19 +89,19 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
 
     if (user) {
-        user.name = req.body.name 
-        user.email = req.body.email
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
 
         if (req.body.password) {
             user.password = req.body.password
         }
 
-        const updateUser = await user.save();
+        const updatedUser = await user.save();
 
         res.status(200).json({
-            _id: updateduser._id,
-            name: updateduser.name,
-            email: updateduser.email,
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
         })
     } else {
         res.status(400);

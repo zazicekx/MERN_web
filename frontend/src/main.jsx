a58e52097e74cb47202b107ef2586d2b8de1dfab
Ondřej Zeinert <zeinert@glancemedia.local>
2023-08-22 16:35:42 +0200

    created app, ve got forms for login and register + main page

---
+++ frontend/src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from 'react-router-dom';
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import HomeScreen from './pages/HomeScreen.jsx';
+import LoginScreen from './pages/LoginScreen.jsx';
+import RegisterScreen from './pages/RegisterScreen.jsx';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={ <App/>}>
+            <Route index={true} path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+        </Route>
+    )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+        <RouterProvider router={ router } />
+  </React.StrictMode>,
+)
